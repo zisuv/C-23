0622662fa342ae1e4ec4a5ac212b7c34db907a41
zisuv <75612773+zisuv@users.noreply.github.com>
2020-12-28 15:56:36 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+class Box{
+  constructor(x,y,width,height){
+      var options ={         
+           restitution:1
+      }
+     this.body = Bodies.rectangle(x,y,width,height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world,this.body);
+  }
+
+display()
+{
+
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0,this.width, this.height);
+    pop();
+}
+
+      
+}
